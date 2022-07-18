cca522582acedb408b71fcc923efee7989f2b1cf
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2022-07-18 13:47:11 +0530

    Add files via upload

---
+++ stripe_payments/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for stripe_payments project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stripe_payments.settings')
+
+application = get_wsgi_application()
